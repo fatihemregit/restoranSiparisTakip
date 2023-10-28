e3d86a4faf81b1a8c6c24467f5f5a8d9d1b87abd
fatihemregit <fatihemre.1111@gmail.com>
2023-10-28 21:11:52 +0300

    ilk commit(sistem değişecek)

---
+++ Form1.Designer.cs
@@ -0,0 +1,432 @@
+﻿namespace RestoranSiparisTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.alanyanum = new System.Windows.Forms.NumericUpDown();
+            this.adiyamannum = new System.Windows.Forms.NumericUpDown();
+            this.tantuninum = new System.Windows.Forms.NumericUpDown();
+            this.sisnum = new System.Windows.Forms.NumericUpDown();
+            this.adananum = new System.Windows.Forms.NumericUpDown();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.sunum = new System.Windows.Forms.NumericUpDown();
+            this.salgamnum = new System.Windows.Forms.NumericUpDown();
+            this.ayrannum = new System.Windows.Forms.NumericUpDown();
+            this.kolanum = new System.Windows.Forms.NumericUpDown();
+            this.soguknum = new System.Windows.Forms.NumericUpDown();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.alanyanum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adiyamannum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tantuninum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sisnum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adananum)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sunum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salgamnum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ayrannum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kolanum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.soguknum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.alanyanum);
+            this.groupBox1.Controls.Add(this.adiyamannum);
+            this.groupBox1.Controls.Add(this.tantuninum);
+            this.groupBox1.Controls.Add(this.sisnum);
+            this.groupBox1.Controls.Add(this.adananum);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(416, 343);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yemekler";
+            // 
+            // alanyanum
+            // 
+            this.alanyanum.Location = new System.Drawing.Point(151, 258);
+            this.alanyanum.Name = "alanyanum";
+            this.alanyanum.Size = new System.Drawing.Size(120, 22);
+            this.alanyanum.TabIndex = 6;
+            this.alanyanum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.alanyanum.Visible = false;
+            // 
+            // adiyamannum
+            // 
+            this.adiyamannum.Location = new System.Drawing.Point(151, 197);
+            this.adiyamannum.Name = "adiyamannum";
+            this.adiyamannum.Size = new System.Drawing.Size(120, 22);
+            this.adiyamannum.TabIndex = 6;
+            this.adiyamannum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.adiyamannum.Visible = false;
+            // 
+            // tantuninum
+            // 
+            this.tantuninum.Location = new System.Drawing.Point(151, 137);
+            this.tantuninum.Name = "tantuninum";
+            this.tantuninum.Size = new System.Drawing.Size(120, 22);
+            this.tantuninum.TabIndex = 6;
+            this.tantuninum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tantuninum.Visible = false;
+            // 
+            // sisnum
+            // 
+            this.sisnum.Location = new System.Drawing.Point(151, 83);
+            this.sisnum.Name = "sisnum";
+            this.sisnum.Size = new System.Drawing.Size(120, 22);
+            this.sisnum.TabIndex = 6;
+            this.sisnum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sisnum.Visible = false;
+            // 
+            // adananum
+            // 
+            this.adananum.Location = new System.Drawing.Point(151, 34);
+            this.adananum.Name = "adananum";
+            this.adananum.Size = new System.Drawing.Size(120, 22);
+            this.adananum.TabIndex = 5;
+            this.adananum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.adananum.Visible = false;
+            this.adananum.ValueChanged += new System.EventHandler(this.adananum_ValueChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(7, 260);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(116, 20);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Alanya kebabı";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(7, 199);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(136, 20);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Adıyaman Kebabı";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 139);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(73, 20);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Tantuni";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 83);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(91, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Şiş Kebab";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Adana Kebab";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.sunum);
+            this.groupBox2.Controls.Add(this.salgamnum);
+            this.groupBox2.Controls.Add(this.ayrannum);
+            this.groupBox2.Controls.Add(this.kolanum);
+            this.groupBox2.Controls.Add(this.soguknum);
+            this.groupBox2.Controls.Add(this.checkBox6);
+            this.groupBox2.Controls.Add(this.checkBox7);
+            this.groupBox2.Controls.Add(this.checkBox8);
+            this.groupBox2.Controls.Add(this.checkBox9);
+            this.groupBox2.Controls.Add(this.checkBox10);
+            this.groupBox2.Location = new System.Drawing.Point(466, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(308, 343);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İçecekler";
+            // 
+            // sunum
+            // 
+            this.sunum.Location = new System.Drawing.Point(128, 34);
+            this.sunum.Name = "sunum";
+            this.sunum.Size = new System.Drawing.Size(120, 22);
+            this.sunum.TabIndex = 5;
+            this.sunum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // salgamnum
+            // 
+            this.salgamnum.Location = new System.Drawing.Point(128, 83);
+            this.salgamnum.Name = "salgamnum";
+            this.salgamnum.Size = new System.Drawing.Size(120, 22);
+            this.salgamnum.TabIndex = 5;
+            this.salgamnum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // ayrannum
+            // 
+            this.ayrannum.Location = new System.Drawing.Point(128, 137);
+            this.ayrannum.Name = "ayrannum";
+            this.ayrannum.Size = new System.Drawing.Size(120, 22);
+            this.ayrannum.TabIndex = 5;
+            this.ayrannum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // kolanum
+            // 
+            this.kolanum.Location = new System.Drawing.Point(128, 258);
+            this.kolanum.Name = "kolanum";
+            this.kolanum.Size = new System.Drawing.Size(120, 22);
+            this.kolanum.TabIndex = 5;
+            this.kolanum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // soguknum
+            // 
+            this.soguknum.Location = new System.Drawing.Point(128, 197);
+            this.soguknum.Name = "soguknum";
+            this.soguknum.Size = new System.Drawing.Size(120, 22);
+            this.soguknum.TabIndex = 5;
+            this.soguknum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(7, 260);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(56, 20);
+            this.checkBox6.TabIndex = 4;
+            this.checkBox6.Text = "Kola";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(7, 199);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(95, 20);
+            this.checkBox7.TabIndex = 3;
+            this.checkBox7.Text = "Soğuk Çay";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(7, 139);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(64, 20);
+            this.checkBox8.TabIndex = 2;
+            this.checkBox8.Text = "Ayran";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(7, 83);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(76, 20);
+            this.checkBox9.TabIndex = 1;
+            this.checkBox9.Text = "Şalgam";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(7, 36);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(45, 20);
+            this.checkBox10.TabIndex = 0;
+            this.checkBox10.Text = "Su";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Masa No";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Toplam Ücret";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Sipariş Girdi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.alanyanum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adiyamannum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tantuninum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sisnum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adananum)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sunum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salgamnum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ayrannum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kolanum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.soguknum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown alanyanum;
+        private System.Windows.Forms.NumericUpDown adiyamannum;
+        private System.Windows.Forms.NumericUpDown tantuninum;
+        private System.Windows.Forms.NumericUpDown sisnum;
+        private System.Windows.Forms.NumericUpDown adananum;
+        private System.Windows.Forms.NumericUpDown sunum;
+        private System.Windows.Forms.NumericUpDown salgamnum;
+        private System.Windows.Forms.NumericUpDown ayrannum;
+        private System.Windows.Forms.NumericUpDown soguknum;
+        private System.Windows.Forms.NumericUpDown kolanum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestoranSiparisTakip
+{
+    public partial class Form1 : Form
+    {
+
+        int toplamTutar = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private int siparisUcretiniOgren(String siparisIsmi)
+        {
+            //burada jsondan sipariş ismine göre sipariş ücretini getiren kodlar yazılacak
+            return 1;
+        }
+
+        private void clickToCheckBox(object sender, EventArgs e,NumericUpDown numer)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            string siparisIsmi = checkBox.Text;
+            //checkbox checklendiğinde fiyatın artma ve azaltma işlemleri
+            if (checkBox.Checked)
+            {
+                numer.Visible = true;
+                numer.Value = 1;
+                //toplamTutar += Convert.ToInt16(numer.Value) * siparisUcretiniOgren(siparisIsmi);
+
+            }
+            else
+            {
+                numer.Visible = false;
+
+                //toplamTutar -= siparisUcretiniOgren(siparisIsmi);
+            }
+        }
+
+        private void tutarHesapla(bool eklemeDurumu,int eklenecekFiyat,int adet) {
+            if (eklemeDurumu) {
+                toplamTutar += eklenecekFiyat * adet;
+            }
+            else
+            {
+                toplamTutar -= eklenecekFiyat * adet;
+            }
+            label3.Text = $": {toplamTutar}";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //adana kebab
+            NumericUpDown  adana = adananum;
+
+
+            clickToCheckBox(sender, e,adana);
+        }
+
+        decimal oldValue;
+
+        private void adananum_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            if(numericUpDown.Value == 0){
+                numericUpDown.Visible = false;
+                toplamTutar = 0;
+                label3.Text = $": {toplamTutar}";
+            }
+            else
+            {
+
+                numericUpDown.Visible = true;
+                int fark = (int)numericUpDown.Value - (int)oldValue;
+                if(fark < 0)
+                {
+                    //değer azalmış
+                    tutarHesapla(false, siparisUcretiniOgren("Adana Kebab"), Math.Abs(fark));
+
+                }
+                else
+                {
+                    //değer artmış
+                    tutarHesapla(true, siparisUcretiniOgren("Adana Kebab"), fark);
+                }
+
+                //if (numericUpDown.Value > oldValue)
+                //{
+                //    //değer artmış
+                //    int fark = (int)numericUpDown.Value - (int)oldValue;
+                //    tutarHesapla(true, siparisUcretiniOgren("Adana Kebab"),fark);
+                //}
+                //else
+                //{
+                //    //değer azalmış
+                //    int fark = (int)oldValue - (int)numericUpDown.Value;
+                //    tutarHesapla(false, siparisUcretiniOgren("Adana Kebab"),fark);
+                //}
+                oldValue = numericUpDown.Value;
+            }
+            
+
+
+
+        }
+    }
+}
